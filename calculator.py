62011d326cca79c6472195cb64c03fe99a5e5fa0
Artix263 <artix2634@gmail.com>
2025-01-21 13:56:45 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def dodawanie(a, b):
+    return a + b
+
+def odjemowanie(a, b):
+    return a - b
+
+def mnnożenie(a, b):
+    return a * b
+
+def dzielenie(a, b):
+    if b != 0:
+      return a/b
+      
